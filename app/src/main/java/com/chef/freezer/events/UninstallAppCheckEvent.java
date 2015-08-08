0673228d7e6ac688495644b6bf16d759f9645c55
brianhellno <brianhellno@gmail.com>
2015-08-08 15:19:24 -0700

    Getting it working on phone.

---
+++ app/src/main/java/com/chef/freezer/events/UninstallAppCheckEvent.java
@@ -0,0 +1,14 @@
+package com.chef.freezer.events;
+import com.chef.freezer.loader.AppCard;
+
+public class UninstallAppCheckEvent
+{
+	
+	public AppCard uaceae;
+
+    public UninstallAppCheckEvent(AppCard AE) {
+        this.uaceae = AE;
+    }
+
+
+}
